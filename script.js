d6d184c7a144f073a0fe9b8ee7d0206ce6790cbe
Only0o <88824887+Only0o@users.noreply.github.com>
2023-05-08 13:36:17 -0300

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function validar() {
     let valor = document.querySelector(".info").value;
     // console.log(valor.value.length);
     if (valor.length == 11 || valor.length == 8) {
-      alert("oi");
       window.location.href = "home_index.html";
     } else {
       alert("Informações invalidas");
