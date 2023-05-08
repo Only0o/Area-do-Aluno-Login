b4c749be7c960b34968be90ed4852e9dd51da0c4
Only0o <88824887+Only0o@users.noreply.github.com>
2023-05-08 13:37:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validar() {
     let valor = document.querySelector(".info").value;
     // console.log(valor.value.length);
     if (valor.length == 11 || valor.length == 8) {
-      window.location.href = "home_index.html";
+      window.location.href = "https://area-do-aluno-home.vercel.app/";
     } else {
       alert("Informações invalidas");
     }
